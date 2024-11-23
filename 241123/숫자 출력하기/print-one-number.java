9837adbf1b9148c9497e64e875b73b74bcf92948
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-11-23 21:29:22 +0900

    Title: 숫자 출력하기, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 241123/숫자 출력하기/print-one-number.java
+++ 241123/숫자 출력하기/print-one-number.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        bw.write(""+3);
+        // bw.write(""+3);
+        bw.write("3");
         
         bw.flush();
         bw.close();
