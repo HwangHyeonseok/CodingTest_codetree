a4dcf0a5340b8ee93d1d48fcd88b6bf0acf65ee3
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-11-27 12:44:57 +0900

    Title: 대문자로 바꾸기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 241127/대문자로 바꾸기/change-to-capital.java
+++ 241127/대문자로 바꾸기/change-to-capital.java
@@ -7,23 +7,42 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        String[][] arr = new String[5][3];
+        // String[][] arr = new String[5][3];
+
+        // for(int r=0; r<5; r++) {
+        //     String input = br.readLine();
+        //     StringTokenizer st = new StringTokenizer(input, " ");
+        //     for(int c=0; c<3; c++) {
+        //         arr[r][c] = st.nextToken();
+        //     }
+        // }
+
+        // // logic // 
+        // for(int r=0; r<5; r++) {
+        //     for(int c=0; c<3; c++) {
+        //         bw.write(String.format("%s ", arr[r][c].toUpperCase())); 
+        //     }
+        //     bw.write("\n");
+        // }
+
+        // logic2 //
+        char[][] arr2 = new char[5][3];
 
         for(int r=0; r<5; r++) {
             String input = br.readLine();
             StringTokenizer st = new StringTokenizer(input, " ");
             for(int c=0; c<3; c++) {
-                arr[r][c] = st.nextToken();
+                arr2[r][c] = st.nextToken().charAt(0);
             }
         }
 
-        // logic // 
         for(int r=0; r<5; r++) {
             for(int c=0; c<3; c++) {
-                bw.write(String.format("%s ", arr[r][c].toUpperCase())); 
+                bw.write((char)(arr2[r][c]-32) + " "); 
             }
             bw.write("\n");
         }
+
 
         bw.flush();
         bw.close();
