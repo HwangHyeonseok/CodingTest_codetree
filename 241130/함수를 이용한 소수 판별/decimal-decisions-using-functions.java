760e63cadcb2b06675c4cb6cd5df697c79546551
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-11-30 16:51:35 +0900

    Title: 함수를 이용한 소수 판별, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241130/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for(int i=a; i<=b; i++) {
+            if(isPrime(i) == true) ans += i;
+        }
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+
+    // 소수 판별 logic //
+    // 7이 소수인지 아닌지 판별하려면, 2~(7/2) 값까지 모두 나눴을 때 나머지가 0이 아니면 된다.
+    // 5 % 2 != 0
+    // 5 % 3 != 0 
+    // -> 따라서 7은 소수이다.
+    static boolean isPrime(int checkNum) {
+        for(int i=2; i<=(checkNum/2); i++) {
+            if(checkNum % i == 0) return false;
+        }
+
+        return true;
+    }
+}
