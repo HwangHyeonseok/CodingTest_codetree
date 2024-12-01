9c01141ea5f0211471dca80d1b04213cfebf8007
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-01 19:39:58 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241201/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int GreatestCommonDivisor = arr[0];
+        for(int i=1; i<N; i++) {
+            GreatestCommonDivisor = lcm(GreatestCommonDivisor,arr[i]);
+        }
+
+        bw.write(GreatestCommonDivisor+"");
+        bw.flush();
+        bw.close();
+    }
+
+    // 최대공약수
+    public static int gcd(int a, int remain) throws IOException{
+        if(remain == 0) return a;
+
+        return gcd(remain, a%remain);
+    }
+    // 최소공배수
+    public static int lcm(int a, int b) throws IOException {
+        return (a*b)/gcd(a,b);
+    }
+}
