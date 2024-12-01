e701beb0f31cc3a168def71baf197d6eba5f35fe
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-01 18:58:40 +0900

    Title: Factorial, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241201/Factorial/factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int ans = fact(N);
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+
+    public static void fact(int N) throws IOException {
+        if(N == 0 || N == 1) return 1;
+        
+        return fact(N-1) * N;
+    }
+}
