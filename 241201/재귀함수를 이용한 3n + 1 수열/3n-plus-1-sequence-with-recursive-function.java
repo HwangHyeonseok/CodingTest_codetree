f39af9b631cf5206fdb46ea31067730d20020176
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-01 19:10:23 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241201/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int ans = logic(N, 0);
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+
+    public static int logic(int n, int cnt) throws IOException {
+        if(n == 1) return cnt;
+        
+        if(n%2==0) return logic(n/2, cnt+1);
+        else return logic(n+3, cnt+1);
+    }
+}
