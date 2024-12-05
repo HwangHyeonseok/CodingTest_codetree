73d7376c27289657ba65d3b3d9af380410286071
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-05 17:05:09 +0900

    Title: Date to Date, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241205/Date to Date/date-to-date.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        // logic // 
+        int[] monthToDay = {31,28,31,30,31,30,31,31,30,31,30,31}; // 1월은 31일, 2월은 28일 ...
+        
+        int OneDay = 0;
+        int TwoDay = 0;
+
+        // 월 계산
+        for(int i=0; i<m1; i++) {
+            OneDay += monthToDay[i];
+        }
+        for(int i=0; i<m2; i++) {
+            TwoDay += monthToDay[i];
+        }
+
+        // 일 계산
+        OneDay += d1;
+        TwoDay += d2;
+
+        // TwoDay - oneDay;
+        int ans = TwoDay - OneDay;
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+}
