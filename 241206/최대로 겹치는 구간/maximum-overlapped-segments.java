645a369a482b2cb22d7ec2c9f81a6e4a96c5e9a7
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-06 13:58:05 +0900

    Title: 최대로 겹치는 구간, Time: 88ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241206/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int MAX_N = 101;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[] board = new Integer[MAX_N];
+        Arrays.fill(board, 0); // 0으로 초기화
+        
+        // for(int i=0; i<board.length; i++) {
+        //     System.out.println(board[i]);
+        // }
+        for(int i=0; i<n; i++) {
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input, " ");
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            
+            for(int c=x1; c<x2; c++) {
+                board[c]++;
+            }
+        }
+
+        
+        // 배열 정렬 후 최댓값 출력
+        Arrays.sort(board, Collections.reverseOrder());
+
+        bw.write(board[0]+"");
+        bw.flush();
+        bw.close();
+    }
+}
