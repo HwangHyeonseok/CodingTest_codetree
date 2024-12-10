4e71392ddfcc776715a5ee0dca43e896bee83843
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2024-12-10 11:54:33 +0900

    Title: 만나는 그 순간, Time: 90ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241210/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,103 @@
+// 매 초마다 어디에 있었는지 기록한다.
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int MAX_N = 1001;
+    static int[] timeToLocationA = new int[MAX_N]; // 시간에 따른 위치를 저장하는 배열
+    static int[] timeToLocationB = new int[MAX_N];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // logic // 
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+        int N = Integer.parseInt(st.nextToken()); // A의 이동 횟수
+        int M = Integer.parseInt(st.nextToken()); // B의 이동 횟수
+        
+        // A 이동 입력 + 구현
+        int timeA = 1; // A의 누적 이동 시간
+        int locationA = 0; // A의 위치
+        for(int i=0; i<N; i++) {
+            input = br.readLine();
+            st = new StringTokenizer(input, " ");
+            String command = st.nextToken();
+            int moveCnt = Integer.parseInt(st.nextToken());
+
+            if(command.equals("R")) {
+                for(int j=0; j<moveCnt; j++) {
+                    locationA++;
+                    timeToLocationA[timeA] = locationA;
+                    timeA++;
+                }
+            }
+
+            else if(command.equals("L")) {
+                for(int j=0; j<moveCnt; j++) {
+                    locationA--;
+                    timeToLocationA[timeA] = locationA;
+                    timeA++;
+                }
+            }
+        }
+
+        // // A 시간별 위치 테스트
+        // for(int i=0; i<20; i++) {
+        //     System.out.print(timeToLocationA[i] + " ");
+        // }
+        // System.out.println();
+
+        // B 이동 입력 + 구현
+        int locationB = 0; // 현재 B의 위치
+        int timeB = 1; // B의 누적 이동 시간
+        for(int i=0; i<M; i++) {
+            input = br.readLine();
+            st = new StringTokenizer(input, " ");
+            String command = st.nextToken();
+            int moveCnt = Integer.parseInt(st.nextToken());
+
+            if(command.equals("R")) {
+                for(int j=0; j<moveCnt; j++) {
+                    locationB++;
+                    timeToLocationB[timeB] = locationB;
+                    timeB++;
+                }
+            }
+
+            else if(command.equals("L")) {
+                for(int j=0; j<moveCnt; j++) {
+                    locationB--;
+                    timeToLocationB[timeB] = locationB;
+                    timeB++;
+                }
+            }
+        }
+
+        // // B 시간별 위치 테스트
+        // for(int i=0; i<20; i++) {
+        //     System.out.print(timeToLocationB[i] + " ");
+        // }
+        // System.out.println();
+
+
+        // output //
+        // 만나는 순간 = 인덱스에 기록한 이동 횟수가 같은 순간 중 가장 짧은 시간이 걸리는 것을 출력
+        int ans = Integer.MAX_VALUE;
+        for(int i=1; i<timeToLocationB.length; i++) {
+            // System.out.println("i : " + i + " A위치 : " + timeToLocationA[i] + " B위치 : " + timeToLocationB[i]);
+            if(timeToLocationA[i] == timeToLocationB[i]) {
+                ans = i;
+                break;
+            }
+        }
+        
+        if(ans == Integer.MAX_VALUE) { // 만날 수 없는 경우
+            ans = -1;
+        }
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+    }
+}
