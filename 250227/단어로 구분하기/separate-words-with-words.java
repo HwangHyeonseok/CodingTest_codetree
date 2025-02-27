179d08174a212a5bb30eeb98c5e48ea25f0ca16c
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-02-27 16:39:01 +0900

    Title: 단어로 구분하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 250227/단어로 구분하기/separate-words-with-words.java
+++ 250227/단어로 구분하기/separate-words-with-words.java
@@ -1,33 +1,57 @@
+// 풀이 2 : split 함수 이용
 import java.util.*;
 import java.io.*;
 
 public class Main {
-    static ArrayList<String> strArr = new ArrayList<>();
+    static String[] strArr;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input1 = br.readLine();
+        String input = br.readLine();
 
-        // logic //
-        String str = "";
-        for(int i=0; i<input1.length(); i++) {
-            if(input1.charAt(i) == ' ') {
-                strArr.add(str);
-                str="";
-            }
-            else {
-                str += input1.charAt(i);
-            }
-        }
-        // 마지막 문자 처리
-        if(str != "") strArr.add(str);
-
+        // logic // 
+        strArr = input.split(" ");
         // output //
-        for(int i=0; i<strArr.size(); i++) {
-            bw.write(strArr.get(i)+"\n");
+        for(int i=0; i<strArr.length; i++) {
+            bw.write(strArr[i]+"\n");
         }
         bw.flush();
         bw.close();
     }
 }
+
+// 풀이 1 : 공백 문자가 나오면 분리해서 넣기
+// import java.util.*;
+// import java.io.*;
+
+// public class Main {
+//     static ArrayList<String> strArr = new ArrayList<>();
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//         String input1 = br.readLine();
+
+//         // logic //
+//         String str = "";
+//         for(int i=0; i<input1.length(); i++) {
+//             if(input1.charAt(i) == ' ') {
+//                 strArr.add(str);
+//                 str="";
+//             }
+//             else {
+//                 str += input1.charAt(i);
+//             }
+//         }
+//         // 마지막 문자 처리
+//         if(str != "") strArr.add(str);
+
+//         // output //
+//         for(int i=0; i<strArr.size(); i++) {
+//             bw.write(strArr.get(i)+"\n");
+//         }
+//         bw.flush();
+//         bw.close();
+//     }
+// }
