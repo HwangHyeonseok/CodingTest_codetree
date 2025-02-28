dff313060359781215b42367f8210bcd058ce4ad
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-02-28 11:45:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

--- 250228/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 250228/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -1,5 +1,5 @@
 // 소수인지 판단
-
+// 소수 : 1이 아닌 수 중에서 나누어 떨어지는 수가 자기 자신인 경우
 import java.util.*;
 import java.io.*;
 
@@ -26,6 +26,7 @@ public class Main {
     }
 
     public static boolean isPrime(int checkNum) {
+        if(checkNum == 1) return false; // 1은 소수가 아니다.
         for(int i=2; i<=checkNum/2; i++) {
             if(checkNum % i == 0) return false;
         }
