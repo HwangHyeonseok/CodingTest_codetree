c589b197da4d17339f874dba7d8504ed876a56cd
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-02-28 12:18:32 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

--- 250228/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 250228/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,9 +1,15 @@
 // idea : 2 4 6 8 5 7 이렇게 입력 받으면 그냥 이 것이 4 6 8 문자열과 일치하는 것이 있는지 화인 
+// [반례]
+// 2 1
+// 22 22
+// 2
 
 import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int[] a = new int[101];
+    static int[] b = new int[101];
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,12 +20,33 @@ public class Main {
         int n1 = Integer.parseInt(st.nextToken());
         int n2 = Integer.parseInt(st.nextToken());
 
-        String input1 = br.readLine();
-        String input2 = br.readLine();
+        input = br.readLine();
+        st = new StringTokenizer(input, " ");
+        for(int i=0; i<n1; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        input = br.readLine();
+        st = new StringTokenizer(input, " ");
+        for(int i=0; i<n2; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
         // logic // 
         String ans = "No";
-        
-        if(input1.contains(input2) == true) ans = "Yes";
+        for(int i=0; i<=n1-n2; i++) {
+            boolean isCombo = true; // 연속부분수열인지 체크
+            for(int j=0; j<n2; j++) { // n2개를 본다.
+                if(a[i+j] != b[j]) {
+                    isCombo = false;
+                    break;
+                }
+            }
+
+            if(isCombo == true) {
+                ans = "Yes";
+                break;
+            }
+        }
         
 
         // output //
