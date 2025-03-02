e24029f9b4ed68515b8c00f17e2e959e9251d429
HwangHyeonseok <51507287+HwangHyeonseok@users.noreply.github.com>
2025-03-02 14:00:09 +0900

    Title: 007, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250302/007/007.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static class People {
+        String code;
+        String location;
+        int time;
+        public People() {
+            this.code="";
+            this.location="";
+            this.time=0;
+        }
+
+        public People(String code, String location, int time) {
+            this.code = code;
+            this.location = location;
+            this.time = time;
+        }
+    };
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input, " ");
+
+        // logic // 
+        String s1 = st.nextToken();
+        String s2 = st.nextToken();
+        int t = Integer.parseInt(st.nextToken());
+        People p = new People(s1,s2,t);
+        
+        // output //
+        bw.write(String.format("secret code : %s\n", p.code));
+        bw.write(String.format("meeting point : %s\n", p.location));
+        bw.write(String.format("time : %d\n", p.time));
+        bw.flush();
+        bw.close();
+    }
+}
